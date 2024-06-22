fd1f14f17783fa590d12675db4051a4057a2e25c
toryoto <s1f102002345@iniad.org>
2024-06-22 21:02:12 +0900

    ポケモン一覧取得API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
+import { getAllPokemon } from './utils/pokemon';
 import './App.css'
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(initialURL);
+      console.log(res)
+    };
+    fetchPokemonData();
+  }, [])
+
   return <div className='App'></div>
 }
 
