977a3583ded7003d12314edd996a6f144e5a8c94
toryoto <s1f102002345@iniad.org>
2024-06-22 22:07:35 +0900

    ポケモン一体一体の詳細を表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getAllPokemon, getPokemon } from './utils/pokemon';
 import './App.css'
+import Card from './components/Card/Card';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
@@ -19,10 +20,10 @@ function App() {
   const loadPokemon = async (data) => {
     // Promise.allは複数の非同期処理を並行して実行して、すべての結果を待つ
     let _pokemonData = await Promise.all(
-      data.map((pokemon) => {
+      data.map(async (pokemon) => {
         // それぞれのポケモンの詳細（画像・タイプなど）を取得する
-        let pokemonRecord = getPokemon(pokemon.url);
-        return pokemonRecord
+        let pokemonRecord = await getPokemon(pokemon.url);
+        return pokemonRecord;
       })
     )
     setPokemonData(_pokemonData);
@@ -36,7 +37,11 @@ function App() {
         <h1>ロード中・・・</h1>
       ) : (
         <>
-          <h1>ポケモンデータを取得しました</h1>
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />;
+            })}
+          </div>
         </>
       )}
     </div>
